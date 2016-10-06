720d80b1299c42b9f08a9294e74e41003174f9a9
Alan Palazzolo <github@zzolo.org>
2016-10-06 15:22:02 -0500

    Getting slack sign in to work with lots of hacks.

--- index.js
+++ index.js
@@ -3,47 +3,44 @@
  */
 
 // Dependencies
-var express = require('express');
-var passport = require('passport');
-var SlackStrategy = require('passport-slack').Strategy;
+const path = require('path');
+const util = require('util');
+const express = require('express');
+const passport = require('passport');
+const slack = require('./lib/slack.js');
 require('dotenv').config({ silent: true });
 
 // Use Slack for auth
-passport.use(new SlackStrategy({
-    clientID: process.env.SLACK_CLIENT_ID,
-    clientSecret: process.env.SLACK_CLIENT_SECRET,
-    scope: 'identity.basic',
-    extendedUserProfile: false
-  },
-  function(accessToken, refreshToken, profile, done) {
-    User.findOrCreate({ SlackId: profile.id }, function (err, user) {
-      return done(err, user);
-    });
-  }
-));
+// See: https://api.slack.com/docs/sign-in-with-slack
+passport.use('slack', slack.strategy);
 
 // Create a new Express application.
 var app = express();
 
 // Configure view engine to render EJS templates.
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
+
+// Setup static files
+app.use(express.static(path.join(__dirname, 'static')));
 
 // Use application-level middleware for common functionality, including
 // logging, parsing, and session handling.
-app.use(require('morgan')('combined'));
+//app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({
   secret: process.env.SESSION_SECRET || 'this should probably be updated, eh',
-  resave: false,
-  saveUninitialized: false
+  saveUninitialized: true,
+  resave: true,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 14
+  }
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-// Setup static files
-app.use(express.static('static'));
-
-// Routes
+// Slack auth routes
 app.get('/auth/slack', passport.authorize('slack'));
 app.get('/auth/slack/callback',
   passport.authorize('slack', { failureRedirect: '/login' }),
@@ -52,22 +49,43 @@ app.get('/auth/slack/callback',
     res.redirect('/');
   });
 
+// Login
+app.get('/login', function(req, res, next) {
+  console.log(req.session);
+  res.render('login', { user: req.user });
+});
+
+// Logout
 app.get('/logout',
-  passport.authorize('slack', { failureRedirect: '/login' }),
+  slack.isLoggedIn,
   function(req, res) {
-  req.session.destroy();
   req.logout();
+  req.session.destroy();
   res.redirect('/');
 });
 
+// Main page
 app.get('/',
-  passport.authorize('slack', { failureRedirect: '/login' }),
+  slack.isLoggedIn,
   function(req, res) {
-  res.render('home', { user: req.user });
+  res.render('home', { user: req.session.user });
 });
 
-app.get('/login', function(req, res) {
-  res.render('login', { user: req.user });
+
+// General error handling
+app.use(function errorHandler(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  if (process.env.NODE_ENV !== 'production') {
+    res.json(err);
+    throw new Error(err);
+  }
+  else {
+    res.status(500);
+    res.render('error', { error: err });
+  }
 });
 
 // Listen
